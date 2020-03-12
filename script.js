1e555a2a596bc4ea2e032a25d45300a2630ed7f7
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2020-03-12 16:57:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const twitter = document.getElementById('twitter');
+const github = document.getElementById('github');
+const link1 = document.getElementById('link1');
+const link2 = document.getElementById('link2');
+const link3 = document.getElementById('link3');
+const link4 = document.getElementById('link4');
+const link5 = document.getElementById('link5');
+//ON
+twitter.addEventListener('mouseenter', () => {
+　　document.getElementById("text").innerHTML="Twitter"
+}, false);
+
+//OFF
+twitter.addEventListener('mouseleave', () => {
+  　　document.getElementById("text").innerHTML=""
+}, false);
+
+  //ON
+github.addEventListener('mouseenter', () => {
+　　document.getElementById("text").innerHTML="Github"
+}, false);
+
+//OFF
+github.addEventListener('mouseleave', () => {
+  　　document.getElementById("text").innerHTML=""
+}, false);
+
+//ON
+link1.addEventListener('mouseenter', () => {
+  　　document.getElementById("text").innerHTML="Nakano's portfolio 2019"
+}, false);
+  
+//OFF
+link1.addEventListener('mouseleave', () => {
+    　　document.getElementById("text").innerHTML=""
+}, false);
+
+//ON
+link2.addEventListener('mouseenter', () => {
+  　　document.getElementById("text").innerHTML="リンク2"
+}, false);
+  
+//OFF
+link2.addEventListener('mouseleave', () => {
+    　　document.getElementById("text").innerHTML=""
+}, false);
+
+//ON
+link3.addEventListener('mouseenter', () => {
+  　　document.getElementById("text").innerHTML="リンク3"
+}, false);
+  
+//OFF
+link3.addEventListener('mouseleave', () => {
+    　　document.getElementById("text").innerHTML=""
+}, false);
+
+//ON
+link4.addEventListener('mouseenter', () => {
+  　　document.getElementById("text").innerHTML="リンク4"
+}, false);
+  
+//OFF
+link4.addEventListener('mouseleave', () => {
+    　　document.getElementById("text").innerHTML=""
+}, false);
+
+//ON
+link5.addEventListener('mouseenter', () => {
+  　　document.getElementById("text").innerHTML="リンク5"
+}, false);
+  
+//OFF
+link5.addEventListener('mouseleave', () => {
+    　　document.getElementById("text").innerHTML=""
+}, false);
+function page(pass){
+  window.location.href = pass;
+}
